18d10a99b465e97b972753f2f415514afc85cf93
tab1k <tabigatkarbayev@icloud.com>
2025-01-10 10:07:04 +0500

    change to blacks

--- unitrade/settings.py
+++ unitrade/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-n7w+l+j&hni476&m=nr$x4i3z)y3j(uewikpn^1yw37^$qh5y7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', '192.168.0.11']
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', '192.168.0.13']
 
 
 # Application definition
